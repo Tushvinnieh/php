c84ef3099b5a3cebb9ee515843a26acf99e56c9e
vincent Mwenda <vincentmwenda63@gmail.com>
2022-08-01 13:44:12 +0300

    My first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello World";
+$x=5;
+$y=5;
+$sum=$x+$y;
+ echo "The sum of $x and $y". $sum;
+ echo $x+$y;
+?>
+
+
---
+++ vinnie.php
@@ -0,0 +1,22 @@
+<?php
+echo "My name is Vinnie <br>";
+$x=5;
+$y=3;
+$sum=$x+$y;
+echo "The sum of $x and $y is " .$sum;
+$z=4545;
+var_dump ($z);
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
